177e1e39fbe560761c4e15d21c5e7f4f36cb0f61
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-22 13:09:59 +0100

    done 6 exercise

---
+++ week-02/day-3/greet.py
@@ -0,0 +1,10 @@
+# - Create variable named `al` and assign the value `Greenfox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Greenfox`
+# - Greet `al`
+
+al = "greenfox"
+
+def greet(x="Greetings, dear"):
+    print(x, al)
+greet()
