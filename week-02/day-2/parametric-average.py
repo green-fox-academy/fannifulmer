40b77e84d6a9e509cdb402acaea03d4438358e9d
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-25 23:51:47 +0100

    parmetric-average start

---
+++ week-02/day-2/parametric-average.py
@@ -0,0 +1,9 @@
+// Write a program that asks for a number.
+// It would ask this many times to enter an integer,
+// if all the integers are entered, it should print the sum and average of these
+// integers like:
+//
+// Sum: 22, Average: 4.4
+
+x = int(input("Say a number: "))
+y = int(input)
