4e6a91ae1defe3b7d863e0322df057552d54208a
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-26 23:17:56 +0200

    working on new problems

---
+++ week-02/day-4/isinlist.py
@@ -0,0 +1,14 @@
+# Check if list contains all of the following elements: 4,8,12,16
+# Create a function that accepts listOfNumbers as an input
+# it should return "true" if it contains all, otherwise print "false"
+
+listOfNumbers = [2, 4, 6, 8, 10, 12, 14, 16]
+list = [4, 8, 12, 16]
+def numberfinder():
+    for i in range(len(listOfNumbers)):
+        if list 
+        return True
+return False
+
+
+numbergenerator()
