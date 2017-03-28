e3f2de6fd4aa00217e5d5a0b07fac0cf610c2a37
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-28 17:07:16 +0200

    converted list to strings

--- week-03/day-2/task-04/crypto_4encoded.py
+++ week-03/day-2/task-04/crypto_4encoded.py
@@ -1,8 +1,17 @@
 # Create a method that decrypts texts/encoded_zen_lines.txt
 my_file = open("encoded-lines.txt", "r")
-my_file = my_file.read()
+my_file = my_file.readlines()
+newfile = []
 
-x = ord("a")
-print(my_file)
+for line in my_file:
+   for c in line:
+       newfile.append(c)
+       #newfile += ord(i)
+#for i in range(1, len(my_file)):
+    #newfile += ord(i)
+    #my_file = my_file.split
+    #newfile += ord(my_file)
+#x = ord("a")
+print(newfile)
 #def decrypt(file_name):
 #    pass
