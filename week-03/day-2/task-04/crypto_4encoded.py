aeb7b7cbb287bd78ca8a0e3ddb0578def5dc534e
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-28 17:29:35 +0200

    Made lines in crypto_4

--- week-03/day-2/task-04/crypto_4encoded.py
+++ week-03/day-2/task-04/crypto_4encoded.py
@@ -4,14 +4,24 @@ my_file = my_file.readlines()
 newfile = []
 
 for line in my_file:
-   for c in line:
-       newfile.append(c)
-       #newfile += ord(i)
+    print(line)
+
+
+
+#for newline in line:
+
+    #newfile = ord(line)
+#line += ord(line)
+#print(line)
+ #  for c in line:
+#       newfile.append(c)
+#print(newfile)
+
+
 #for i in range(1, len(my_file)):
     #newfile += ord(i)
     #my_file = my_file.split
     #newfile += ord(my_file)
 #x = ord("a")
-print(newfile)
 #def decrypt(file_name):
 #    pass
