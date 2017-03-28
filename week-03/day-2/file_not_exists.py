6042679d1f19d6ceba4cf75827df3172b0a95723
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-28 16:31:39 +0200

    file-not-exists task completed

--- week-03/day-2/file_not_exists.py
+++ week-03/day-2/file_not_exists.py
@@ -1,9 +1,13 @@
 # write a function that takes a filename and returns the number of lines the
 # file contains. It should return zero if the file does not exist.
+try:
+    def file_founder():
+        name = input("Please select a file: ")
+        my_file = open(name)
+        my_file = my_file.readlines()
 
-#the_file = input("Please select a file: ")
-my_file = open("duplicated-chars.txt")
-my_file = my_file.readlines()
-
-x = len(my_file)
-print(x)
+        x = len(my_file)
+        print(x)
+    file_founder()
+except FileNotFoundError:
+    print("0")
