6e1a2bd740bc4d50feaa21a5d1c9d56cedc7d9e1
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-29 22:11:31 +0200

    made the outline for checkerboard

--- week-03/day-3/checkerboard.py
+++ week-03/day-3/checkerboard.py
@@ -5,11 +5,24 @@ root = Tk()
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-def checkboard (x, color):
-    bsquare = canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-    wsquare = canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+#def checkboard (x, color):
+#    bsquare = canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+#    wsquare = canvas.create_rectangle(x1, y1, x2, y2, fill=color)
 
-color = ["black", "white"]
+w = 50
+x = 0
+y = 0
+
+canvas.create_rectangle(x, y, w, w)
+for row in range(7):
+    for col in range(7):
+        canvas.create_rectangle(x, y, w, w)
+        x = x + w
+    y = y + w
+    x = 0
+
+
+'''color = ["black", "white"]
 x1 = 0
 y1 = 0
 x2 = 50
@@ -17,10 +30,10 @@ y2 = 50
 x = 300
 for i in range(len(color)):
     checkboard(x, color[i])
-    x -= 300/x
+    x -= 300/len(color)
     x1 += 50
     y2 += 50
 
-# fill the canvas with a checkerboard pattern.
+# fill the canvas with a checkerboard pattern.'''
 
 root.mainloop()
