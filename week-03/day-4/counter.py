a8e8f0b94851f82eb4e18c53751f18833a0772bd
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-30 11:11:17 +0200

    counter finished

---
+++ week-03/day-4/counter.py
@@ -0,0 +1,11 @@
+# Write a recursive function that takes one parameter: n and counts down from n.
+
+n = 3
+def fractal_counter(n):
+    print(n)
+    if n == 0:
+        return 0
+    else:
+        return n, fractal_counter(n-1)
+
+(fractal_counter(n))
