30fb4a49f5b8a8ede96f93be540b566ba419e08d
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-30 19:33:05 +0200

    fibonacci finished

--- week-03/day-4/fibonacci.py
+++ week-03/day-4/fibonacci.py
@@ -4,3 +4,12 @@
 # previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
 # and so on. Define a recursive fibonacci(n) method that returns the nth
 # fibonacci number, with n=0 representing the start of the sequence.
+
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci (3))
