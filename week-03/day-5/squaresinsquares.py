c3dec10ea878367c651032a332d44d26e411b2f5
Fanni Fulmer <fannifulmer@gmail.com>
2017-03-31 10:59:39 +0200

    they look like turtles

--- week-03/day-5/squaresinsquares.py
+++ week-03/day-5/squaresinsquares.py
@@ -5,19 +5,19 @@ root = Tk()
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-def make_a_square(x, y, size):
+def make_a_square(x, y, size, width):
     canvas.create_rectangle(x, y, x+size, y+size, fill='', width=20)
 
 
-def making_six_sguares(x, y, size):
-    make_a_square(100, 100+size, 230)
+def making_six_sguares(x, y, size, width):
+    make_a_square(x, y, size, width)
     if size > 5:
-        making_six_sguares(x+size/3, y, size/3)
-        making_six_sguares(x+size*2/3, y+size/3, size/3)
-        making_six_sguares(x, y+size/3, size/3)
-        making_six_sguares(x+size/3, y+size*2/3, size/3)
+        making_six_sguares(x+100, y+100, size/3, width)
+        making_six_sguares(x+size*2/3, y+size/3, size/3, width-4)
+        making_six_sguares(x, y+size/3, size/3, width-8)
+        making_six_sguares(x+size/3, y+size*2/3, size/3, width-16)
         canvas.update()
 
-making_six_sguares(0, 0, 230)
+making_six_sguares(0, 0, 230, 20)
 
 root.mainloop()
