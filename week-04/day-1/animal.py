5dd70855e7aa03f938c1471edea42c07e8b2d1e8
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-03 14:11:04 +0200

    animal completed

---
+++ week-04/day-1/animal.py
@@ -0,0 +1,27 @@
+'''Create Animal class
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one'''
+
+class Animal:
+    hunger = 50
+    thirst = 50
+
+    def eat(self):
+        self.hunger -= 1
+
+    def drink(self):
+        self.thirst -= 1
+
+    def play(self):
+        self.thirst += 1
+        self.hunger += 1
+
+zebra = Animal()
+zebra.eat()
+zebra.drink()
+print(zebra.hunger)
+print(zebra.thirst)
