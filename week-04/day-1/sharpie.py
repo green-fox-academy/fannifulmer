2e2c28587177686cebc4ed455b799ec21eee7e05
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-03 15:17:22 +0200

    counter complated

---
+++ week-04/day-1/sharpie.py
@@ -0,0 +1,22 @@
+'''Create Sharpie class
+We should know about each sharpie their color (which should be a string), width (which will be a floating point number), ink_amount (another floating point number)
+When creating one, we need to specify the color and the width
+Every sharpie is created with a default 100 as ink_amount
+We can use() the sharpie objects
+which decreases inkAmount'''
+
+class Sharpie:
+     ink_amount = 100
+
+     def __init__(self, color, width):
+         self.color = str(color)
+         self.width = float(width)
+
+     def use(self):
+         self.ink_amount -= 1
+
+
+pinksharpie = Sharpie('pink', 0.5)
+pinksharpie.use()
+
+print(pinksharpie.ink_amount)
