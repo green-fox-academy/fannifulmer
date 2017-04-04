448eab9781af25668c6102dc64ae0e53ad63af37
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-04 17:18:14 +0200

    in garden exercise, printed out the flowers

--- week-04/day-2/garden.py
+++ week-04/day-2/garden.py
@@ -14,29 +14,35 @@ when watering it the tree can only absorb the 40% of the water
 eg. watering with 10 the tree's amount of water should only increase with 4'''
 
 class Garden():
-    def __init__(self, color, water_amount = 0):
-        self.color = color
-
     def watering(self, water_level):
-        self.watering = (water_level)/len(needs_water)
+        self.watering = (water_level)/len()
 
 
 class Flower(Garden):
     def __init__(self, color, water_amount = 0):
         self.color = color
         self.water_amount = water_amount
+        #self.needs_water = needs_water
 
-    def add_water(self, flower):
+    def add_water(self, water_amount = 0):
         if water_amount < 5:
-            self.needs_water.addend(flower)
+            print("The", self.color, "needs water")
+            #self.needs_water.addend(flower)
 
-    def absorb_water(self, flower)
-        needs_water = []
+    def absorb_water(self):
+        water_amount += (water_level*0.75)
 
-class Tree(Garden):
+'''class Tree(Garden):
     def __init__(self, color, water_amount = 0):
         super().__init__(color, water_amount)
 
     def add_water(self, tree):
         if water_amount < 10:
-            self.needs_water.addend(tree)
+            self.needs_water.addend(tree)'''
+
+watering = Garden()
+rose = Flower('red', 0)
+lily = Flower('white', 0)
+lily.add_water()
+rose.add_water()
+print()
