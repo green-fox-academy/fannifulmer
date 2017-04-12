01bd712f55c3c037e78522e1f123e1d713834ad4
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-12 14:41:06 +0200

    working on the scrum_game

---
+++ week-05/day-3/scrum_game.py
@@ -0,0 +1,85 @@
+from tkinter import *
+
+class Game():
+    def __init__(self):
+        root = Tk()
+        canvas = Canvas(root, width = 720, height = 720)
+        game = MapOfTheGame(root, canvas)
+        game.draw_tiles()
+        hero = Hero(root, canvas)
+        canvas.pack()
+        canvas.bind("<KeyPress>", hero.on_key_press)
+        canvas.focus_set()
+        root.mainloop()
+        
+
+class MapOfTheGame():
+
+    def __init__(self, root, canvas):
+        self.root = root
+        self.canvas = canvas
+                
+    def draw_tiles(self):
+        map = [
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,1,0,0,0],
+        [0,0,1,1,0,0,1,0,0,0],
+        [0,0,0,0,0,0,1,0,0,0],
+        [1,1,1,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,0,1,1,0,0],
+        [0,0,0,1,0,0,1,1,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+        ]
+        self.floor_img = PhotoImage(file="floor.png")
+        self.wall_img = PhotoImage(file="wall.png")
+        for row in range(len(map)):
+            for cell in range(len(map[row])):
+                if map[cell][row] == 0:
+                    self.canvas.create_image(row*72, cell*72, anchor=NW, image=self.floor_img)
+                else:
+                    self.canvas.create_image(row*72, cell*72, anchor=NW, image=self.wall_img)
+                    
+class Hero():
+    def __init__(self, root, canvas):
+        self.root = root
+        self.canvas = canvas
+        self.hero_down = PhotoImage(file="hero-down.png")
+        self.hero_right = PhotoImage(file="hero-right.png")
+        self.hero_left = PhotoImage(file="hero-left.png")
+        self.hero_up = PhotoImage(file="hero-up.png")
+        self.x = 0
+        self.y = 0
+        self.hero_drawer()
+
+    def hero_drawer(self):
+        self.canvas.create_image(self.x, self.y, anchor = NW, image=self.hero_down)
+        
+    def on_key_press(self, e):
+        self.e = e
+        if self.e.keycode == 38:
+            if self.y >= 0:
+                self.y = self.y - 72
+                self.hero_drawer()
+            else:
+                self.y = self.y
+        elif self.e.keycode == 40:
+            if self.y >= 0:
+                self.y = self.y + 72
+                self.hero_drawer()
+            else:
+                self.y = self.y
+        elif self.e.keycode == 39:
+            if self.x >= 0:
+                self.x = self.x + 72
+                self.hero_drawer()
+            else:
+                self.x = self.x
+        elif self.e.keycode == 37:
+            if self.x > 0:
+                self.x = self.x - 72
+                self.hero_drawer()
+            
+                    
+game = Game()
