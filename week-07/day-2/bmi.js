f6754325d88146f7c92644892cd2483a9675095e
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-02 14:21:12 +0200

    done exercises 7-9

---
+++ week-07/day-2/bmi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var massInKg = 81.2;
+var heightInM = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+var BMI = Math.pow(heightInM, 2)
+var BMI = massInKg / BMI
+console.log(BMI)
