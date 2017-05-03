b8815c07b267593a87d80a332a8fe73749c136ea
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-03 13:03:39 +0200

    feat(sum):created sum function

---
+++ week-07/day-3/sum.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Write a function called `sum` that sum all the numbers until the given parameter
+// - The function should return the results
+
+function sum(numbers) {
+    var summed = 0;
+    for (var i = 0; i <= numbers; i++){
+        summed += i;
+    }
+    return summed;
+}
+
+console.log(sum(1));
