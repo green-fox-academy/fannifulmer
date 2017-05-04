2fc7a6291c37082dc44969ec761058333ea05aff
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-04 15:52:59 +0200

    practiced innerHTML and textContetnt

---
+++ week-07/day-4/content-3.js
@@ -0,0 +1,3 @@
+var out1 = document.querySelector('p');
+document.querySelector('.output1').innerHTML = out1.textContent;
+document.querySelector('.output2').innerHTML = out1.innerHTML;
---
+++ week-07/day-4/content-4.js
@@ -0,0 +1,7 @@
+var list = ['apple', 'banana', 'cat', 'dog'];
+
+var first = document.querySelectorAll('li')
+first[0].innerHTML = list[0];
+first[1].innerHTML = list[1];
+first[2].innerHTML = list[2];
+first[3].innerHTML = list[3];
