0d48d68efee05ef682a0163a917c1fd5108e99f6
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-04 15:25:13 +0200

    feat(content-2): completed all tasks

---
+++ week-07/day-4/content-2.js
@@ -0,0 +1,7 @@
+var dolphin = document.querySelector('.dog')
+
+document.querySelector('.cat').innerHTML = dolphin.textContent;
+document.querySelector('.balloon').innerHTML = dolphin.textContent;
+document.querySelector('.apple').innerHTML = dolphin.textContent;
+
+
