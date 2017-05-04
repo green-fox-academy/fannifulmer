9ecca35beba1e556449878ed074881a8c0449e73
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-04 17:43:27 +0200

    createelement-1 exercise done

---
+++ week-07/day-4/createelement-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var astList = document.querySelector('.asteroids');
+var fox = document.createElement('li');
+var fox2 = document.createElement('li');
+fox.textContent = 'The Green Fox';
+fox2.textContent = 'The Lamplighter';
+astList.appendChild(fox);
+astList.appendChild(fox2);
+
+var headCont = document.querySelector('.container');
+headCont.innerHTML = 'I can add elements to the DOM!';
+
+var headContImg = document.createElement('img');
+headCont.appendChild(headContImg);
+console.log(headContImg);
+headContImg.setAttribute('src', "https://static.wixstatic.com/media/f4461b_83457ca5dd844c71a760d36e6583d0ff.png/v1/fill/w_168,h_168,al_c,usm_0.66_1.00_0.01/f4461b_83457ca5dd844c71a760d36e6583d0ff.png");
