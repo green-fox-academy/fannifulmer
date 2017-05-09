445441dff0a90f2169945c779317150125c63627
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-09 15:57:12 +0200

    feat(petrolstation):completed task

--- week-08/day-2/cars.js
+++ week-08/day-2/cars.js
@@ -3,14 +3,14 @@
 // Call the ride method of the volvo, with 42 as a parameter
 
 var volvo = {
-  type: "Volvo",
-  fuel: 23,
-  consumption: 0.06,
-  kms: 43000,
-  ride: function (km) {
-    this.kms += km;
-    this.fuel -= km * this.consumption; 
-    return this.fuel
+    type: "Volvo",
+    fuel: 23,
+    consumption: 0.06,
+    kms: 43000,
+    ride: function (km) {
+        this.kms += km;
+        this.fuel -= km * this.consumption; 
+        return this.fuel
   }
 };
 console.log(volvo.ride(42));
@@ -21,13 +21,13 @@ console.log(volvo.ride(42));
 // 52 as a parameter
 
 var ferrari = {
-  type: "Ferrari",
-  fuel: 0,
-  consumption: 0.12,
-  kms: 9000,
-  ride: function (km) {
-    this.kms += km;
-    this.fuel -= km * this.consumption; 
+    type: "Ferrari",
+    fuel: 0,
+    consumption: 0.12,
+    kms: 9000,
+    ride: function (km) {
+        this.kms += km;
+        this.fuel -= km * this.consumption; 
   }
 };
 
