2a47dd239c87fc08cb9927db7c68d9c36a7ae671
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-10 11:19:31 +0200

    feat(call): loged 20 factorial

---
+++ week-08/day-3/call.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function factorialTillLimitWithCallback(limit, callback) {
+  var factorial = 1;
+  for (var i = 1; i <= limit; ++i) {
+    callback(factorial);
+    factorial *= i;
+  }
+}
+
+function callback(callthis) {
+    console.log(callthis)
+}
+
+factorialTillLimitWithCallback(20, callback)
+
+// Create a function and pass it as a parameter to the factorialTillLimitWithCallback
+// function, and print all the factorial numbers till 20
+
+// function provideName(callback) {
+//   callback('John Doe');
+// }
+// 
+// function printName(name) {
+//   console.log(name);
+// }
+// 
+// provideName(printName); // prints John Doe
