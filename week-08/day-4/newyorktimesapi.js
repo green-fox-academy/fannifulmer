3b998cec395cb5b470741784498e9cfa001137f8
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-11 16:37:37 +0200

    feat(newyorktimes): added headline and snippet

---
+++ week-08/day-4/newyorktimesapi.js
@@ -0,0 +1,30 @@
+'use strict';
+var newyorkTimes =  new XMLHttpRequest();
+var wrapper = document.querySelector('.wrapper')
+newyorkTimes.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=moon+landing+by+Apollo+11&api_key=2cccf4def763418d933793e8a1f01630', true);
+
+newyorkTimes.onreadystatechange = function () {
+        if(newyorkTimes.readyState === XMLHttpRequest.DONE && newyorkTimes.status === 200) {
+            var resp = JSON.parse(newyorkTimes.response);
+            headlinesCreater(resp.response.docs);
+            console.log(resp);
+            }
+        }
+newyorkTimes.send();
+
+var headlinesCreater = function(headerData) {
+    for (let i = 0; i < 9; i++){
+        let listheadlines = document.createElement('li')
+        let headlines = document.createElement('a');
+        let snippet = document.createElement('p');
+        snippet.classList.add("snippet");
+        headlines.innerHTML = headerData[i].headline.main;
+        snippet.innerHTML = headerData[i].snippet;
+        headlines.setAttribute('href', headerData[i].headline.main);
+        wrapper.appendChild(listheadlines);
+        listheadlines.appendChild(headlines);
+        listheadlines.appendChild(snippet);
+        console.log(headlines);
+    }
+}
+
