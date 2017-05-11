8aef4ff205d8babc7397111eb6b7ae43e827c271
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-11 13:32:50 +0200

    feat(giphy): made pictures apper

--- week-08/day-4/giphy.js
+++ week-08/day-4/giphy.js
@@ -1,14 +1,19 @@
 'use strict';
 var rainRequest =  new XMLHttpRequest();
+var pictureContainer = document.querySelector('.wrapper');
 rainRequest.open('GET', 'http://api.giphy.com/v1/gifs/search?q=rain&api_key=dc6zaTOxFJmzC', true);
 rainRequest.onreadystatechange = function () {
         if(rainRequest.readyState === XMLHttpRequest.DONE && rainRequest.status === 200) {
-                // console.log(rainRequest.response);
-                var resp = JSON.parse(rainRequest.response);
-                for (let i = 0; i <= 16; i++){
-                console.log(resp.data[i].images.original_still.url);
+            var resp = JSON.parse(rainRequest.response);
+            imageAdder(resp.data)
             }
         }
-    };
 rainRequest.send();
 
+var imageAdder = function(images) {
+    for (let j = 0; j <= 16; j++){
+        var pictures = document.createElement('img');
+        pictures.setAttribute('src', images[j].images.original_still.url)
+        pictureContainer.appendChild(pictures);
+    }
+}
