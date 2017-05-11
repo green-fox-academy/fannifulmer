a186cb7429496cc096d3de9b5baacb528390f66d
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-11 13:58:42 +0200

    saving before trying changes

--- week-08/day-4/giphy.js
+++ week-08/day-4/giphy.js
@@ -1,19 +1,30 @@
 'use strict';
 var rainRequest =  new XMLHttpRequest();
 var pictureContainer = document.querySelector('.wrapper');
+var imageContainer = document.querySelectorAll('img');
 rainRequest.open('GET', 'http://api.giphy.com/v1/gifs/search?q=rain&api_key=dc6zaTOxFJmzC', true);
 rainRequest.onreadystatechange = function () {
         if(rainRequest.readyState === XMLHttpRequest.DONE && rainRequest.status === 200) {
             var resp = JSON.parse(rainRequest.response);
-            imageAdder(resp.data)
+            console.log(resp);
+            imageAdder(resp.data);
             }
         }
 rainRequest.send();
 
-var imageAdder = function(images) {
+var imageAdder = function(rainImages) {
     for (let j = 0; j <= 16; j++){
         var pictures = document.createElement('img');
-        pictures.setAttribute('src', images[j].images.original_still.url)
+        pictures.setAttribute('src', rainImages[j].images.original_still.url);
+        pictures.addEventListener('click', onSwitchGif);
         pictureContainer.appendChild(pictures);
     }
+}
+
+var onSwitchGif = function(gifVideos){
+    for (let k = 0; k <= 16; k++){
+        imageContainer[k].setAttribute('src', gifVideos[k].images.downsized_medium
+        .url);
+    }
+
 }
