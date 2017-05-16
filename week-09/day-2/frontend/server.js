77dd889426e7b79ba647fb93b3a185a5693f0e8f
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-16 15:47:41 +0200

    feat(greeter): completed task

--- week-09/day-2/frontend/server.js
+++ week-09/day-2/frontend/server.js
@@ -9,13 +9,48 @@ app.get('/', function(req, res){
 
 app.use('/assets', express.static('assets'));
 
-// app.get('/doubling/:input', function(req, res){
-//     
-// })
-// app.get('/five/:lang', function(req, res){
-//     var lang = req.params.lang;
-//     var fiveFunc = five[lang];
-//     res.send(fiveFunc());
+app.get('/doubling', function(req, res){
+    if (req.query === {} || req.query.input === undefined){
+        res.send({
+            error: "Please provide an input!"
+        })
+    } else {
+        res.send({
+            received: parseInt(req.query.input),
+            result: req.query.input*2
+        });
+    }
+});
+
+
+// app.get('/greeter/:name/:title', function(req, res){
+//     var name = req.params.name;
+//     var title = req.params.title;
+//     if (req.query === {} || req.params.name === undefined){
+//         res.send({
+//             error: "Please provide a name!"
+//         })
+//     } else {
+//         res.send({
+//             name: req.query.name,
+//             title: req.query.title
+//         });
+//     }
+// });
+app.get('/greeter', function(req, res){
+    if (req.query.name === undefined){
+        res.send({
+            error: "Please provide a name!"
+        });
+    } else if (req.query.title === undefined){
+        res.send({
+            error: "Please provide a title!"
+        });
+    } else {
+        res.send({
+            welcome_message: "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!"
+        });
+    }
 });
 
 app.listen(8080);
