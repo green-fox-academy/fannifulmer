ae665142f7931c38017f5e0a87942beb08817b53
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-19 10:47:32 +0200

    feat(redditbackend): lists out data in a json file

--- week-09/day-5/redditbackend.js
+++ week-09/day-5/redditbackend.js
@@ -4,6 +4,27 @@ const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "reddit"
+});
+
+var result = {'posts': 0}
+
+app.get('/posts', function(req, res){
+    conn.query('SELECT * FROM posts', function(err, rows){
+        if(err) {
+            console.log("PARA", err); 
+        } else {
+            result = rows;
+        }
+        res.send(result);
+    })
+})
+
+
 app.get('/hello', function(req, res){
     var hello = function() {
         return 'hello World';
