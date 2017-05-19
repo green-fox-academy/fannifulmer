4d83447ee371fe4b6b025b480a9774797cacb518
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-19 09:40:31 +0200

    feat(redditbackend): set up backend, returns hello to the page

---
+++ week-09/day-5/redditbackend.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mysql = require("mysql");
+const express = require("express");
+const app = express();
+
+app.get('/hello', function(req, res){
+    var hello = function() {
+        return 'hello';
+    }
+    res.send(hello())    
+});
+
+app.listen(3000, function () {
+    console.log('server running');
+})
