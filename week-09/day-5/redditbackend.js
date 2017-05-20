081f1c51966e4a42f19d12fc67b0ad276bee7cf7
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-20 15:02:34 +0200

    feat(redditbackend): now can post and delete

--- week-09/day-5/redditbackend.js
+++ week-09/day-5/redditbackend.js
@@ -3,10 +3,12 @@
 const mysql = require("mysql");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require('cors');
-let result = {'posts': 0}
+let result = {'posts': 0};
 
 app.use(cors());
+app.use(bodyParser.json());
 
 var conn = mysql.createConnection({
   host: "localhost",
@@ -15,25 +17,54 @@ var conn = mysql.createConnection({
   database: "reddit"
 });
 
-
-app.get('/posts', function(req, res){
-    conn.query('SELECT * FROM posts', function(err, rows){
-        if(err) {
-            console.log("PARA", err); 
-        } else {
-            result = rows;
-        }
-        res.send(result);
-    })
-})
-
-
 app.get('/hello', function(req, res){
     var hello = function() {
         return 'hello World';
     }
     res.send(hello())    
 });
+
+app.get('/posts', function(req, res){
+    conn.query('SELECT * FROM posts', function(err, rows){
+        if(err) {
+            console.log("PARA", err); 
+        } else {
+            result.posts = rows;
+        }
+        res.send(result);
+    })
+})
+
+app.post('/posts', function(req, res){
+    let timestamp = Math.floor(Date.now() / 1000);
+    let resp = {};
+    conn.query('INSERT INTO posts (title, href, timestamp) VALUES( ' + '"' + req.body.title + '"' + ', "' + req.body.href + '"' + ',"' + timestamp + '");'  ,function(err,rows){
+        conn.query('SELECT * FROM posts WHERE title="' +  req.body.title + '"', function(err, rows){
+            if(err) {
+                console.log("PARA", err); 
+            } else {
+                resp = rows;
+            }
+            res.send(resp);
+        })
+    })
+})
+
+
+app.delete('/posts/:id', function(req, res){
+    let respDel = {};
+    conn.query('DELETE FROM posts WHERE id="' + req.params.id + '"' ,function(err,rows){
+        conn.query('SELECT * FROM posts', function(err, rows){
+            if(err) {
+                console.log("PARA", err); 
+            } else {
+                respDel = rows;
+            }
+            res.send(respDel);
+        })
+    })
+});
+
 
 app.listen(3000, function () {
     console.log('server running');
