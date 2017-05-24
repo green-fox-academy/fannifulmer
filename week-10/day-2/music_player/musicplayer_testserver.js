eabdb27b7a5fa8b359b2915a880877d102d6a498
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-24 12:08:37 +0200

    feat(musicplayer_testserver): json file added to server

--- week-10/day-2/music_player/musicplayer_testserver.js
+++ week-10/day-2/music_player/musicplayer_testserver.js
@@ -3,11 +3,14 @@
 const express = require("express");
 const app = express();
 
-app.get('/', function(req, res){
-    var hello = function() {
-        return 'hello World';
-    }
-    res.send(hello())    
+app.get('/playlists', function(req, res){
+    var dataServer = [
+	{ "id": 1, "title": "Favorites", "system": 1},
+	{ "id": 2, "title": "Music for programming", "system": 0},
+	{ "id": 3, "title": "Driving", "system": 0},
+	{ "id": 5, "title": "Fox house", "system": 0},
+];
+    res.send(dataServer)
 });
 
 
