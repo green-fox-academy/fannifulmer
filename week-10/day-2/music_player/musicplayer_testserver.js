cd24adfe3a1ce79f5a9541c28a76398338021fca
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-24 14:38:22 +0200

    started to creat front-end for musicplayer

--- week-10/day-2/music_player/musicplayer_testserver.js
+++ week-10/day-2/music_player/musicplayer_testserver.js
@@ -3,6 +3,12 @@
 const express = require("express");
 const app = express();
 
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/musicplayer.html');
+});
+
 app.get('/playlists', function(req, res){
     var dataServer = [
 	{ "id": 1, "title": "Favorites", "system": 1},
@@ -11,6 +17,14 @@ app.get('/playlists', function(req, res){
 	{ "id": 5, "title": "Fox house", "system": 0},
 ];
     res.send(dataServer)
+});
+
+app.get('/playlist-tracks', function(req, res){
+    var playlistDataServer = [
+    	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+    	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
+    ];
+        res.send(playlistDataServer)
 });
 
 
