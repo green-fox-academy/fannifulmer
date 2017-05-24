c5c8f37020a52017b32968b34992ad6cefb8363a
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-24 16:46:11 +0200

    nailed mustache

--- week-10/day-2/music_player/assets/ajax.js
+++ week-10/day-2/music_player/assets/ajax.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var url = 'http://localhost:3000/playlists';
+const playlists = document.querySelector('.playlists');
 
 var getPlaylists = function(callback) {
     var xhr =  new XMLHttpRequest();
@@ -10,11 +11,17 @@ var getPlaylists = function(callback) {
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             var resp = JSON.parse(xhr.response);
             console.log(resp);
-            // callback(resp);
+            callback(resp);
         }
     }
     xhr.send();
 }
 
+var renderPlaylists = function(response) {
+    let output = Mustache.render("{{#tracks}} <li class='listStyle'>{{title}}<span class='deleter'>X</span></li> {{/tracks}}", {tracks:response});
+    console.log({tracks:response});
+    console.log(output);
+    playlists.innerHTML = output;
+}
 
-getPlaylists();
+getPlaylists(renderPlaylists);
