d1deb8a73e8c70c3f99e9abf2e6fb5e1c1ce7180
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-25 16:14:08 +0200

    added event listener to the tracks

--- week-10/day-2/music_player/assets/ajax.js
+++ week-10/day-2/music_player/assets/ajax.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const playlists = document.querySelector('.playlists');
-const tracklists = document.querySelector('.tracks');
+const tracklists = document.querySelector('.trackUl');
 const currentlyPlay = document.querySelector('.currently_playing');
 const deleter = document.querySelectorAll('.deleter');
 
@@ -18,6 +18,7 @@ const getTracks = function(callback) {
     const endpoint = 'http://localhost:3000/playlist-tracks';
     callback(endpoint, function(resp) {
         renderTracks(resp);
+        audioControll();
     });
 }
 
@@ -52,11 +53,7 @@ const renderPlaylists = function(response) {
 const renderTracks = function(response) {
     const tracksOutput = Mustache.render("{{#tracks}} <li class='clicked_track'><span>1.</span><a>{{title}}</a><span>{{duration}}</span></li> {{/tracks}}", {tracks:response});
     tracklists.innerHTML = tracksOutput;
-    let onClickTrack = document.querySelectorAll('.clicked_track');
-    // console.log(onClickTrack[1]);
-    // onClickTrack.addEventListener('click', function(){
-    //     console.log('alma');
-    // })
+    // let onClickTrack = document.querySelectorAll('.clicked_track');
 }
 
 const currentPlayingSong = function(response){
@@ -69,6 +66,15 @@ const renderCurrentPlay = function(response) {
     currentlyPlay.innerHTML = "<h1 class='current_song'>" + response[0].title + "</h1><h4 class='current_artist'>" + response[0].artist + "</h4>"
 }
 
+const audioControll = function() {
+    let currentSong = document.querySelectorAll('.clicked_track');
+    currentSong.forEach(function(track){
+        track.addEventListener('click', function(){
+            Array.from(track.parentNode.children).indexOf(track)
+            console.log(track);
+        })   
+    })    
+}
 
 getPlaylists(ajax)
 getTracks(ajax);
