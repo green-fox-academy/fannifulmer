d4951467518cf5965b6834105c693bb705c1e27f
Fanni Fulmer <fannifulmer@gmail.com>
2017-05-25 14:45:28 +0200

    created real backend for my musicplayer with sql database

--- week-10/day-2/music_player/assets/ajax.js
+++ week-10/day-2/music_player/assets/ajax.js
@@ -2,20 +2,29 @@
 
 const playlists = document.querySelector('.playlists');
 const tracklists = document.querySelector('.tracks');
-const currentlyPlay = document.querySelector('.current_song');
+const currentlyPlay = document.querySelector('.currently_playing');
+const deleter = document.querySelectorAll('.deleter');
 
 
 const getPlaylists = function(callback) {
     const endpoint = 'http://localhost:3000/playlists';
     callback(endpoint, function(resp) {
-        renderPlaylists(resp);
+        console.log(resp);
+        renderPlaylists(resp.playlists);
     });
 }
 
 const getTracks = function(callback) {
     const endpoint = 'http://localhost:3000/playlist-tracks';
     callback(endpoint, function(resp) {
-        renderTracks(resp);
+        renderTracks(resp.tracks);
+    });
+}
+
+const getCurrent = function(callback) {
+    const endpoint = 'http://localhost:3000/playlist-tracks';
+    callback(endpoint, function(resp) {
+        renderCurrentPlay(resp);
     });
 }
 
@@ -26,7 +35,6 @@ const ajax = function(url, callback) {
     xhr.onreadystatechange = function() {
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             var resp = JSON.parse(xhr.response);
-            console.log(resp);
             callback(resp);
         }
     }
@@ -34,34 +42,31 @@ const ajax = function(url, callback) {
 }
 
 const renderPlaylists = function(response) {
+    console.log(response);
     const output = Mustache.render("{{#playlists}} <li class='listStyle'>{{title}}<span class='deleter'>X</span></li> {{/playlists}}", {playlists:response});
-    console.log({playlists:response});
+    console.log(response);
     playlists.innerHTML = output;
-    console.log(output);
 }
+
 
 const renderTracks = function(response) {
-    let songDuration = getTime(response[0].duration)
-    const tracksOutput = Mustache.render("{{#tracks}} <li class='listStyle'><span>1.</span><a>{{title}}</a><span>" + songDuration + "</span></li> {{/tracks}}", {tracks:response});
-    console.log({tracks:response});
+    // let songDuration = getTime(response[0].duration)
+    const tracksOutput = Mustache.render("{{#tracks}} <li class='listStyle'><span>1.</span><a>{{title}}</a><span>{{duration}}</span></li> {{/tracks}}", {tracks:response});
     tracklists.innerHTML = tracksOutput;
-    console.log(tracksOutput);
 }
 
-const render currentPlay = function(response) {
-    const currentPlayOutput = Mustache.render("{{#currentPlay}}<h1 class="current_song">Purple Drift</h1>{{/currentPlay}}", {currentPlay:response}); {currentPlay:response});
-    console.log({currentPlay:response});
-    currentlyPlay.innerHTML = tracksOutput;
-    console.log(tracksOutput);
-}
+// const renderCurrentPlay = function(response) {
+//     const currentPlayOutput = Mustache.render("{{#currentPlay}}<h1 class='current_song'>{{title}}</h1><h4 class='current_artist'>{{artist}}</h4>{{/currentPlay}}", {currentPlay:response});
+//     currentlyPlay.innerHTML = currentPlayOutput;
+// }
+// 
+// const getTime = function(time) {
+//     var minutes = "0" + Math.floor(time / 60);
+//     var seconds = "0" + time % 60;
+//     return minutes + ":" + seconds;
+// }
 
-const getTime = function(time) {   
-    console.log(time);
-    var minutes = "0" + Math.floor(time / 60);
-    var seconds = "0" + time % 60;
-    console.log(minutes);
-    return minutes + ":" + seconds;
-}
 
 getPlaylists(ajax)
 getTracks(ajax);
+// getCurrent(ajax);
---
+++ week-10/day-2/music_player/musicplayer_backend.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+var result = {'playlists': 0};
+var trackResult = {'tracks': 0};
+
+app.use('/assets', express.static('assets'));
+app.use(cors());
+app.use(bodyParser.json());
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "musicplayer"
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/musicplayer.html');
+});
+
+app.get('/playlists', function(req, res){
+    conn.query('SELECT * FROM playlists', function(err, rows){
+        if(err) {
+            console.log("PARA", err); 
+        } else {
+            result.playlists = rows;
+        }
+        res.send(result);
+    })
+});
+
+app.get('/playlist-tracks', function(req, res){
+    conn.query('SELECT * FROM tracks', function(err, rows){
+        if(err) {
+            console.log("PARA", err); 
+        } else {
+            trackResult.tracks = rows;
+        }
+        res.send(trackResult);
+    })
+});
+
+
+app.listen(3000, () => {console.log('server is running')});
