154784817d602eb380663395b43d3cfe18f1ac54
Fanni Fulmer <fannifulmer@gmail.com>
2017-06-01 14:10:13 +0200

    working on the front-end, did some refactoring

--- week-10/todo_app/assets/todo_front.js
+++ week-10/todo_app/assets/todo_front.js
@@ -1,23 +1,41 @@
 'use strict';
+const todo_container = document.querySelector('.todo_item');
 
-class ajaxQuest {
-    constructor(method, url) {
-        this.method = method;
-        this.url = url;
-    }
-    ajax(method, url){
-        const xhr =  new XMLHttpRequest();
-        xhr.open(method, url, true);
-    
+const ajaxQuest = function () {
+    const ajax = function(method, url, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+
         xhr.onreadystatechange = function() {
             if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                var resp = JSON.parse(xhr.response);
-                console.log(resp);
+                var response = JSON.parse(xhr.response);
+                callback(response);
             }
         }
         xhr.send();
-    }
-}
+    };
 
-var getTodos = new ajaxQuest();
-getTodos.ajax('GET', 'http://localhost:3000/todos');
+    return {
+        ajax
+    };
+};
+
+const renderTodos = function(){
+    const ajax = ajaxQuest();
+    
+    const renderTodo = function() {
+        const url = 'http://localhost:3000/todos'
+        ajax.ajax('GET', url, function(response) {
+            console.log(response);
+        });
+
+    }
+    return {
+        renderTodo
+    };
+};
+
+const app = renderTodos();
+app.renderTodo();
+
+
