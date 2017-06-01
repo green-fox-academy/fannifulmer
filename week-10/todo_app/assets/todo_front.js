f4987dc97d3693125ff58cadeeff6c4306c4ded8
Fanni Fulmer <fannifulmer@gmail.com>
2017-06-01 17:33:13 +0200

    can post finally

--- week-10/todo_app/assets/todo_front.js
+++ week-10/todo_app/assets/todo_front.js
@@ -1,10 +1,11 @@
 'use strict';
-const todo_container = document.querySelector('.todo_item');
+
+const button = document.querySelector('button');
 
 const ajaxQuest = function () {
     const ajax = function(method, url, callback) {
         const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
+        xhr.open(method, url, true);
 
         xhr.onreadystatechange = function() {
             if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -20,22 +21,71 @@ const ajaxQuest = function () {
     };
 };
 
-const renderTodos = function(){
-    const ajax = ajaxQuest();
-    
-    const renderTodo = function() {
-        const url = 'http://localhost:3000/todos'
-        ajax.ajax('GET', url, function(response) {
-            console.log(response);
-        });
+const ajaxPost = function() {
+    const inputArea = document.querySelector('.input_box')
+    const ajaxP = function(method, url, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
 
-    }
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                var response = JSON.parse(xhr.response);
+                console.log(callback);
+                callback(response);
+            }
+        }
+        var newInput = {
+            title: inputArea.value
+        }
+        xhr.send(JSON.stringify(newInput));
+    };
     return {
-        renderTodo
+        ajaxP
     };
 };
 
-const app = renderTodos();
-app.renderTodo();
+
+const getTodos = function(){
+    const ajax = ajaxQuest();
+    const post = ajaxPost();
+    const renderer = renderTodos();
+    
+    const getTodo = function() {
+        const url = 'http://localhost:3000/todos'
+        ajax.ajax('GET', url, function(response) {
+            renderer.renderData(response);
+        });
+    }
+        
+    const postTodo = function() {
+        const url2 = 'http://localhost:3000/todos'
+        button.addEventListener('click', function(){
+            post.ajaxP('POST', url2, function(response) {
+                getTodo(response);
+            })
+        });
+    }
+
+    return {
+        getTodo, 
+        postTodo
+    };
+};
 
 
+const renderTodos = function(){
+    const todo_container = document.querySelector('.todo_item');
+    
+    const renderData = function(response) {
+        const output = Mustache.render("{{#todos}} <div><h2>{{title}}</h2><img src='assets/trash_icon.png'><input type='checkbox'></div> {{/todos}}", {todos:response});
+        todo_container.innerHTML = output;
+    }
+    return {
+        renderData
+    }
+}
+
+const app = getTodos();
+app.getTodo();
+app.postTodo();
--- week-10/todo_app/todo_backend.js
+++ week-10/todo_app/todo_backend.js
@@ -2,8 +2,15 @@
 
 const express = require("express");
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 let result;
+let results;
 
 app.use('/assets', express.static('assets'));
 
@@ -26,17 +33,32 @@ app.get('/todos', function(req, res){
             result = rows;
         }
         res.send(result);
-    })
+    });
 });
 
-// app.get('/todos/:id', function(req, res){
-//     conn.query('SELECT * FROM todos', function(err, rows){
-//         if(err) {
-//             console.log("PARA", err); 
-//         } else {
-//             result = rows;
-//         }
-//         res.send(result);
-// })
+app.get('/todos/:id', function(req, res){
+    conn.query('SELECT * FROM todos WHERE id = ?', req.params.id, function(err, rows){
+        if(err) {
+            console.log("PARA", err); 
+        } else {
+            results = rows;
+            if (results.length > 0){
+                res.send(results);    
+            } else {
+                res.send('There is no such todo');
+            };
+        };
+    });
+});
 
-app.listen(3000, () => {console.log('server is running')});
+app.post('/todos', function(req, res){
+    console.log('backend');
+    conn.query('INSERT INTO todos (title) VALUES("' + req.body.title + '");'  ,function(err,rows){
+        res.send(result);
+    });
+    console.log('something happend');
+});
+
+
+
+app.listen(3000, () => {console.log('server is running')})
